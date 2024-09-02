1daa5b3dedb86e005bb6267d6d6f8c5970b7c065
SZH0728 <sun060728@126.com>
2024-09-02 16:01:36 +0800

    新增动画标题和条件展示组件该组件在右上角显示一个标题，包括传入的动画总数和当前页面的标题。 优化了用户界面，使信息展示更加直观。

---
+++ frontend/src/component/ShowTitleAndCondition.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <!-- 右上角标题 -->
+  <div class="d-flex align-items-center">
+    <h2 class="text-info">{{ title }}</h2>
+    <span class="ms-2 text-secondary">(一共{{ totalAnime }}部动画)</span>
+  </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+const props = defineProps({
+  title: String,
+  totalAnime: Number
+})
+</script>
