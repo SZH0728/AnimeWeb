466b7105558ada112a8c6cf4dfa98308e5305ce2
SZH0728 <sun060728@126.com>
2024-09-02 16:01:51 +0800

    新增`ShowLoadingCondition.vue`组件以处理加载状态和错误消息该组件使用Vue的Composition API构建，包含两个状态属性：`loading`和`errorMessage`。它根据这些属性显示不同的反馈信息给用户，如加载指示器或错误消息。

---
+++ frontend/src/component/ShowLoadingCondition.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="loading" class="alert alert-info d-flex w-100">
+    <strong>正在加载...</strong>
+  </div>
+  <div v-if="errorMessage.length !== 0" class="alert alert-danger d-flex w-100">
+    <strong>错误!</strong> 访问数据失败: {{ errorMessage }}
+  </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+defineProps({
+  loading: Boolean,
+  errorMessage: String
+})
+</script>
