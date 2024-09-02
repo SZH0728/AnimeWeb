21606b761962cb0e20a78f04bbbabf98c7a320dd
SZH0728 <sun060728@126.com>
2024-09-02 16:47:32 +0800

    新建搜索视图组件，实现基于关键词的动画搜索功能。该组件展示搜索结果列表，总结果数及分页导航，同时处理加载状态和错误消息显示。

---
+++ frontend/src/views/SearchView.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container-fluid vh-100 d-flex flex-column">
+
+    <div class="d-flex justify-content-between align-items-center p-3">
+      <ShowTitleAndCondition :title="'搜索'" :totalAnime=totalAnime />
+    </div>
+
+    <ShowLoadingCondition :loading=loading :errorMessage=errorMessage />
+    <div v-if="!loading && !errorMessage && totalAnime === 0" class="alert alert-primary d-flex w-100">
+      <strong>什么都没有找到...</strong>
+    </div>
+
+    <!-- 主体部分 -->
+    <div class="flex-fill d-flex flex-column justify-content-center align-items-center">
+      <AnimeList :data=data />
+      <Paginate :totalPage=totalPage :changePage=getData />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted, watch} from 'vue';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+
+import Paginate from "@/component/Paginate.vue";
+import ShowLoadingCondition from "@/component/ShowLoadingCondition.vue";
+import AnimeList from "@/component/AnimeList.vue";
+import ShowTitleAndCondition from "@/component/ShowTitleAndCondition.vue";
+
+// 获取参数
+const route = useRoute();
+const keyword = ref(route.params.keyword);
+
+watch(() => route.params.keyword, (newKeyword) => {
+  keyword.value = newKeyword;
+  getData(1);
+});
+
+// 定义数据信息
+const totalAnime = ref(0)
+const totalPage = ref(0)
+
+// 定义数据状态
+const data = ref([]);
+const loading = ref(false);
+
+// 定义错误信息
+const errorMessage = ref('');
+
+async function getData(page) {
+  loading.value = true;
+
+  // 清空数据
+  data.value = [];
+  totalAnime.value = 0
+  totalPage.value = 0
+
+  try {
+    const response = await axios.get(`http://127.0.0.1:8000/anime/search?keyword=${keyword.value}&page=${page}`);
+    data.value = response.data.data;
+    totalAnime.value = response.data['total'];
+    totalPage.value = response.data['total_page'];
+  } catch (error) {
+    // 处理错误
+    console.error('Error fetching data:', error);
+    errorMessage.value = error.message;
+  }
+
+  loading.value = false;
+}
+
+onMounted(() => {
+  getData(1);
+});
+</script>
+
+<style scoped>
+
+</style>
